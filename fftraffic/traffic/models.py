5b514c9257238eb47043a4787a8b0723e962fd5b
Alexey Londkevich <londkevich@gmail.com>
2017-11-16 03:39:52 +0100

    Add Table

--- fftraffic/traffic/models.py
+++ fftraffic/traffic/models.py
@@ -6,6 +6,13 @@ POKER_ROOM = (
     ('PS', 'PokerStars'),
 )
 
+GAMES = (
+    ('NL', "NL Hold'em"),
+    ('PLO', "PL Omaha"),
+    ('PLO8', "PL Omaha H/L"),
+    ('NLO8', "NL Omaha H/L"),
+)
+
 
 class Country(models.Model):
 
@@ -26,3 +33,30 @@ class Player(models.Model):
 
     def __str__(self):
         return "{} ({})".format(self.name, self.country)
+
+
+class Table(models.Model):
+    room = models.CharField(max_length=3, choices=POKER_ROOM)
+    name = models.CharField(max_length=15)
+    game = models.CharField(max_length=4, choices=GAMES)
+    limit = models.IntegerField()
+
+    class Meta:
+        unique_together = ('room', 'name')
+
+    def __str__(self):
+        return "{} ({} {})".format(self.name, self.game, self.limit)
+
+
+class Scan(models.Model):
+    pass
+
+
+class TableScan(models.Model):
+    pass
+
+
+class PlayerScan(models.Model):
+    pass
+
+
