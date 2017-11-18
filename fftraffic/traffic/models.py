22da6be665320a0738343b9ce834bb7dc3146f05
Alexey Londkevich <londkevich@gmail.com>
2017-11-18 22:57:52 +0100

    Add last_can to Table

--- fftraffic/traffic/models.py
+++ fftraffic/traffic/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-POKER_ROOM = (
+ROOMS = (
     ('PS', 'PokerStars'),
 )
 
@@ -15,7 +15,6 @@ GAMES = (
 
 
 class Country(models.Model):
-
     name = models.CharField(unique=True, max_length=20)
 
     def __str__(self):
@@ -23,10 +22,9 @@ class Country(models.Model):
 
 
 class Player(models.Model):
-
-    room = models.CharField(max_length=3, choices=POKER_ROOM)
+    room = models.CharField(max_length=3, choices=ROOMS, default=ROOMS[0][0])
     name = models.CharField(max_length=15)
-    country = models.OneToOneField(Country)
+    country = models.ForeignKey(Country)
 
     class Meta:
         unique_together = ('room', 'name')
@@ -36,10 +34,10 @@ class Player(models.Model):
 
 
 class Table(models.Model):
-    room = models.CharField(max_length=3, choices=POKER_ROOM)
+    room = models.CharField(max_length=3, choices=ROOMS, default=ROOMS[0][0])
     name = models.CharField(max_length=15)
-    game = models.CharField(max_length=4, choices=GAMES)
-    limit = models.IntegerField()
+    game = models.CharField(max_length=4, choices=GAMES, default=GAMES[0][0])
+    limit = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ('room', 'name')
@@ -47,21 +45,31 @@ class Table(models.Model):
     def __str__(self):
         return "{} ({} {})".format(self.name, self.game, self.limit)
 
+    @property
+    def last_scan(self):
+        return self.scans.last()
+
 
 class Scanner(models.Model):
     ip = models.GenericIPAddressField(unique=True)
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return "{}".format(self.ip)
+
 
 class Scan(models.Model):
     scanner = models.ForeignKey(Scanner, related_name='scans')
     datetime = models.DateTimeField(auto_now_add=True)
-    room = models.CharField(max_length=3, choices=POKER_ROOM)
+    room = models.CharField(max_length=3, choices=ROOMS, default=ROOMS[0][0])
+
+    def __str__(self):
+        return "{} {}".format(self.scanner, self.datetime)
 
 
 class TableScan(models.Model):
-    scan = models.ForeignKey(Scan)
-    table = models.ForeignKey(Table)
+    scan = models.ForeignKey(Scan, related_name='tables')
+    table = models.ForeignKey(Table, related_name='scans')
     player_count = models.SmallIntegerField(default=0)
     average_pot = models.FloatField(default=0.0)
     players_per_flop = models.SmallIntegerField(default=0)
@@ -69,9 +77,16 @@ class TableScan(models.Model):
     unique_player_count = models.SmallIntegerField(default=0)
     entry_count = models.SmallIntegerField(default=0)
 
+    def __str__(self):
+        return "{} players={}".format(self.table, self.player_count)
+
 
 class PlayerScan(models.Model):
-    player = models.ForeignKey(Player)
-    entries = models.SmallIntegerField()
+    player = models.ForeignKey(Player, related_name='scans')
+    table_scan = models.ForeignKey(TableScan, related_name='players')
+    entries = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return "{} entries={}".format(self.player, self.entries)
 
 
--- fftraffic/traffic/tests/test_models.py
+++ fftraffic/traffic/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from traffic.models import Country, Player
+from traffic.models import Country, Player, Table, Scanner, Scan, TableScan, PlayerScan
 
 # Create your tests here.
 
@@ -14,3 +14,32 @@ class PlayerTest(TestCase):
         self.assertEqual(player.name, 'putin')
         self.assertEqual(player.room, 'PS')
         self.assertEqual(player.country.name, 'Albania')
+
+
+class ScanTest(TestCase):
+
+    def setUp(self):
+        self.albania = Country.objects.create(name='Albania')
+        self.nigeria = Country.objects.create(name='Nigeria')
+        self.pushkin = Player.objects.create(name='pushkin', country=self.nigeria)
+        self.obama = Player.objects.create(name='obama', country=self.nigeria, room='PS')
+        self.lenin = Player.objects.create(name='lenin', country=self.albania, room='PS')
+        self.aquarium = Table.objects.create(name='Aquarium')
+        self.kino = Table.objects.create(name='Kino')
+        self.scanner = Scanner.objects.create(ip='192.168.0.1', name='main')
+        self.scan_0 = Scan.objects.create(scanner=self.scanner)
+        self.scan_1 = Scan.objects.create(scanner=self.scanner)
+        self.table_scan_0_aquarium = TableScan.objects.create(scan=self.scan_0, table=self.aquarium, player_count=2)
+        self.table_scan_0_kino = TableScan.objects.create(scan=self.scan_0, table=self.kino, player_count=2)
+        self.table_scan_1_aquarium = TableScan.objects.create(scan=self.scan_1, table=self.aquarium, player_count=3)
+        self.table_scan_1_kino = TableScan.objects.create(scan=self.scan_1, table=self.kino, player_count=3)
+
+    def test_scan(self):
+        self.assertEqual(self.scanner.scans.all()[0], self.scan_0)
+        self.assertEqual(self.scanner.scans.all()[1], self.scan_1)
+        self.assertEqual(list(self.aquarium.scans.all()), [self.table_scan_0_aquarium, self.table_scan_1_aquarium])
+
+    def test_table_last_scan(self):
+        self.assertEqual(self.aquarium.last_scan, self.table_scan_1_aquarium)
+        self.assertEqual(self.kino.last_scan, self.table_scan_1_kino)
+
