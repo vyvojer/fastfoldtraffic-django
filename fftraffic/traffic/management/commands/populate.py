2814a2a035a61f97ab8fe86b8f130f2cf0d83f7f
Alexey Londkevich <londkevich@gmail.com>
2017-12-07 05:51:40 +0100

    Add populating countries

--- fftraffic/traffic/management/commands/populate.py
+++ fftraffic/traffic/management/commands/populate.py
@@ -1,21 +1,28 @@
 from django.core.management.base import BaseCommand, CommandError
-from traffic.models import Table
-from traffic.allowed import ALLOWED_TABLES
+from traffic.models import Table, Country
+from traffic.allowed import ALLOWED_TABLES, ALLOWED_COUNTRIES
 
 
 class Command(BaseCommand):
     help = 'Populate tables and countries'
 
     def handle(self, *args, **options):
-        for table_allowed in ALLOWED_TABLES:
-            room = table_allowed[0]
-            name = table_allowed[1]
-            game = table_allowed[2]
-            limit = table_allowed[3]
-            max_players = table_allowed[4]
+        for allowed_table in ALLOWED_TABLES:
+            room = allowed_table[0]
+            name = allowed_table[1]
+            game = allowed_table[2]
+            limit = allowed_table[3]
+            max_players = allowed_table[4]
             table, _ = Table.objects.get_or_create(room=room, name=name)
             table.game = game
             table.limit = limit
             table.max_players = max_players
             table.save()
+
+        for allowed_country in ALLOWED_COUNTRIES:
+            name = allowed_country[0]
+            iso = allowed_country[1]
+            country, _ = Country.objects.get_or_create(iso=iso)
+            country.name = name
+            country.save()
 
