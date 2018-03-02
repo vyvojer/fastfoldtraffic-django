0cd87c028668393a67d989b9d128f4fc4342d5c4
vyvojer <londkevich@gmail.com>
2018-03-02 02:35:24 +0100

    Add TableScan

--- fastfoldtraffic/traffic/admin.py
+++ fastfoldtraffic/traffic/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Player, Table, Scanner, Scan, Country
+from .models import Player, Table, Scanner, Scan, TableScan, Country
 
 # Register your models here.
 
 admin.site.register(Player)
 admin.site.register(Table)
+admin.site.register(TableScan)
 admin.site.register(Scanner)
 admin.site.register(Scan)
 admin.site.register(Country)
