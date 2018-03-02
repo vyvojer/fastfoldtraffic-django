bb041f859cf30f945aa94d0cd9498e594d830d24
vyvojer <londkevich@gmail.com>
2018-03-02 02:30:01 +0100

    Remove sqlite from production, add ALLOWED_HOSTS and ..._COOKIE

--- fastfoldtraffic/fastfoldtraffic/settings/base.py
+++ fastfoldtraffic/fastfoldtraffic/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['192.168.1.150']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
--- fastfoldtraffic/fastfoldtraffic/settings/production.py
+++ fastfoldtraffic/fastfoldtraffic/settings/production.py
@@ -1,12 +1,8 @@
 from .base import *
 
-ALLOWED_HOSTS = ['*']
 
 LOGGING_SENDER_NAME = 'fastfoldtraffic.com'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../../db.sqlite3'),
-    }
-}
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
