8f6146a81511a7a67f5b50bb645e4771ccb7e74f
vyvojer <londkevich@gmail.com>
2018-03-07 03:36:15 +0100

    Add ModelAdmin classes

--- fastfoldtraffic/traffic/admin.py
+++ fastfoldtraffic/traffic/admin.py
@@ -3,11 +3,15 @@ from .models import Player, Table, Scanner, Scan, TableScan, Country
 
 # Register your models here.
 
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'limit', 'last_scan')
+
+
 class TableScanAdmin(admin.ModelAdmin):
     list_display = ('table', 'datetime')
 
 admin.site.register(Player)
-admin.site.register(Table)
+admin.site.register(Table, TableAdmin)
 admin.site.register(TableScan, TableScanAdmin)
 admin.site.register(Scanner)
 admin.site.register(Scan)
