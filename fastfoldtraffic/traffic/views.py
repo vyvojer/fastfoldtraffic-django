e420a11f831e1d0911f03b81d4b9401601a366fe
vyvojer <londkevich@gmail.com>
2018-03-16 00:57:52 +0100

    Remove chart generation from views

--- fastfoldtraffic/traffic/views.py
+++ fastfoldtraffic/traffic/views.py
@@ -15,29 +15,6 @@ from .serializers import ScanSerializer
 # Create your views here.
 
 
-def _render_spark_chart(data: dict, title=None):
-    spark_options = dict(
-        fill=True,
-        width=100,
-        height=20,
-        show_dots=False,
-        show_legend=False,
-        show_x_labels=False,
-        show_y_labels=False,
-        spacing=0,
-        margin=0,
-        min_scale=1,
-        max_scale=2,
-        explicit_size=True,
-        no_data_text='',
-        js=(),
-    )
-    chart = pygal.Line(**spark_options)
-    values = data['values']
-    chart.add(title, values)
-    return chart.render(is_unicode=True)
-
-
 class IndexView(ListView):
     template_name = 'traffic/index.html'
     context_object_name = 'tables'
@@ -57,18 +34,6 @@ class TableView(DetailView):
         room = 'ps'
         name = self.kwargs['table_name'].replace('-', ' ')
         return get_object_or_404(Table, room__iexact=room, name__iexact=name)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        table = context['table']
-        for field in [('unique_player_count', 'Players'),
-                      ('entry_count', 'Entries'),
-                      ('mtr', 'MTR'),
-                      ('players_per_flop', 'Players Per Flop'),
-                      ('average_pot', 'Average Pot'),
-                      ]:
-            context['{}_chart'.format(field[0])] = _render_spark_chart(table.chart(field[0], LAST_24), field[1])
-        return context
 
 
 @api_view(['PUT'])
